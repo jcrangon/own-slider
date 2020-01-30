ee50272d4f36ce35793804fb065fa9bafcbe6024
jcrangon <jc.rangon@gmail.com>
2020-01-30 02:15:59 +0100

    second

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	
+	$("#slider1").ownSliders({
+		showSlides : 4,
+		slideToScroll : 2,
+		delay : 3000,
+		showArrows : true,
+		arrowType : "content",
+		arrowRightContent : "p",
+		arrowRightContentSize : 2,
+		slideShadow : false,
+	});
+	
+	$("#slider2").ownSliders({
+		arrowRightImageURL : "https://cpng.pikpng.com/pngl/s/288-2883770_right-arrow-comments-slider-arrow-icon-png-transparent.png",
+	});
+	
+});
